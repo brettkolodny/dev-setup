fcc15ea75ff169afa7711fa7088da57344000806
brettkolodny <brettkolodny@gmail.com>
2023-07-31 13:00:48 -0400

    Update configs

--- update.sh
+++ update.sh
@@ -1,7 +1,8 @@
 # Copy fleek config
 cp ~/.fleek.yml ./fleek.yml
 # Copy helix config
-cp -R ~/.config/helix ./helix
+cp -R ~/.config/helix/config.toml ./helix/config.toml
+cp -R ~/.config/helix/languages.toml ./helix/languages.toml
 # Copy all npm modules
 ls ~/.npm-packages/bin > npm-global.txt
 
